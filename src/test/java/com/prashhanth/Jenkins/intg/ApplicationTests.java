c932802af904d90724ccb01235afbf9fb8095636
prashhanthsoujanya <prashhanth.nelakanti@gmail.com>
2021-08-15 11:37:42 +0530

    first commit

---
+++ src/test/java/com/prashhanth/Jenkins/intg/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prashhanth.Jenkins.intg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
